320a4a9d5257d016bc374a00d4ac0e5882c88e51
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-06-13 08:47:57 +0100

    Add Response Type to Post Method

--- NCS.DSS.AdviserDetail/PostAdviserDetailHttpTrigger/PostAdviserDetailHttpTrigger.cs
+++ NCS.DSS.AdviserDetail/PostAdviserDetailHttpTrigger/PostAdviserDetailHttpTrigger.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web.Http.Description;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -10,6 +11,7 @@ namespace NCS.DSS.AdviserDetail.PostAdviserDetailHttpTrigger
     public static class PostAdviserDetailHttpTrigger
     {
         [FunctionName("Post")]
+        [ResponseType(typeof(Models.AdviserDetail))]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "AdviserDetails")]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("Post Adviser Detail C# HTTP trigger function processed a request.");
