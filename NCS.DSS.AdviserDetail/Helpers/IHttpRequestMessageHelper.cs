ee6bace1fdaf31528d37a6860d098871a1670492
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-19 15:10:41 +0100

    Add Http Helpers and Validation for Model

---
+++ NCS.DSS.AdviserDetail/Helpers/IHttpRequestMessageHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NCS.DSS.AdviserDetail.Helpers
+{
+    public interface IHttpRequestMessageHelper
+    {
+        Task<T> GetAdviserDetailFromRequest<T>(HttpRequestMessage req);
+    }
+}
