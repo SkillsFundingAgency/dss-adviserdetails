d22eb2e423e9b55cb621462035f40aa545c0b8c6
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2018-08-02 12:06:43 +0100

    Update Regex for name

--- NCS.DSS.AdviserDetail/Models/AdviserDetail.cs
+++ NCS.DSS.AdviserDetail/Models/AdviserDetail.cs
@@ -13,7 +13,7 @@ namespace NCS.DSS.AdviserDetail.Models
 
         [Required]
         [StringLength(100)]
-        [RegularExpression(@"^[a-zA-Z]+((['\,\.\-][a-zA-Z])?[a-zA-Z]*)*$")]
+        [RegularExpression(@"^[a-zA-Z]+(([\s'\,\.\-][a-zA-Z])?[a-zA-Z]*)*$")]
         [Display(Description = "Name of the adviser")]
         [Example(Description = "this is some text")]
         public string AdviserName { get; set; }
