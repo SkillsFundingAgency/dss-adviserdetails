9d004bb823ff8e95616180041242bca2a0be2671
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2019-03-08 13:50:43 +0000

    update app setting

--- NCS.DSS.AdviserDetail/AdviserDetailChangeFeedTrigger/Service/AdviserDetailChangeFeedTriggerService.cs
+++ NCS.DSS.AdviserDetail/AdviserDetailChangeFeedTrigger/Service/AdviserDetailChangeFeedTriggerService.cs
@@ -10,7 +10,7 @@ namespace NCS.DSS.AdviserDetail.AdviserDetailChangeFeedTrigger.Service
     public class AdviserDetailChangeFeedTriggerService : IAdviserDetailChangeFeedTriggerService
     {
         private readonly string _queueName = Environment.GetEnvironmentVariable("ChangeFeedQueueName");
-        private readonly string _serviceBusConnectionString = Environment.GetEnvironmentVariable("ChangeFeedServiceBusConnectionString");
+        private readonly string _serviceBusConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnectionString");
 
         public async Task SendMessageToChangeFeedQueueAsync(Document document)
         {
